e1f7c54fbc56346077530858fa5d816c4b54d164
anggakrnwn <aakurniawan1306@gmail.com>
2025-04-26 18:20:11 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Navbar responsive toggle
+const menuBtn = document.getElementById('menu-btn');
+const mobileMenu = document.getElementById('mobile-menu');
+
+menuBtn.addEventListener('click', () => {
+  mobileMenu.classList.toggle('hidden');
+});
